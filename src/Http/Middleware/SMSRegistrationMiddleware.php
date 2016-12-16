3f3d82ed2bb198687641e03f206baef948a2c268
ZhangGuisheng <tlv591208510@gmail.com>
2016-12-16 19:59:49 +0800

    add ServiceProvider and Middleware

---
+++ src/Http/Middleware/SMSRegistrationMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sliverwing\Registration\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Logging\Log;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\ViewErrorBag;
+
+class SMSRegistrationMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if ($request->session()->get('VerificationCode') !== null && $request->input('VerificationCode') == $request->session()->get('VerificationCode') && $request->input('phone') == $request->session()->get('VerificationPhoneNumber')) {
+            return $next($request);
+        } else {
+            $bag = new MessageBag();
+            $bag->add('VerificationCode', '请检查您的验证码输入');
+            return redirect('/register')->withInput(
+                $request->except('password')
+            )->with('errors', $request->session()->get('errors', new ViewErrorBag())->put('default', $bag));
+        }
+    }
+}
---
+++ src/SMSRegisterationServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sliverwing\Registration;
+
+use Sliverwing\Registration\Http\Middleware\SMSRegistrationMiddleware;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+
+
+class SMSRegisterationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        $router->middleware('smsregistration', SMSRegistrationMiddleware::class);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $basePath = dirname(__DIR__);
+            $this->publishes([
+                $basePath . '/config/smsregistration.php' => config_path('smsregistration.php'),
+            ]);
+        }
+    }
+}
