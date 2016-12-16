3767673658c93e955059a1d7eb5fb0780c0adf89
ZhangGuisheng <tlv591208510@gmail.com>
2016-12-16 18:46:27 +0800

    first blood

---
+++ README.md
@@ -0,0 +1,2 @@
+# Add SMS Validate Before Register For Laravel
+## UNDERCONSTRUCTION! I'm still working on it. (´･ω･`)
---
+++ config/smsregistration.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'alidayuconfigname' => '',
+    'param' => '',
+    'expiration' => 60, //Seconds
+];
