d345ee8eda26b8fd99fa855cabf3823810ec41dd
Sliverwing <tlv591208510@gmail.com>
2017-02-05 11:05:36 +0800

    display TimeToWait, display error message instead of pop-up alert window

--- views/sliverwing/auth/passwords/reset.blade.php
+++ views/sliverwing/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Reset Password</div>
+                    <div class="panel-heading">重置密码</div>
 
                     <div class="panel-body">
                         @if (session('status'))
@@ -18,7 +18,7 @@
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-                                <label for="phone" class="col-md-4 control-label">Phone Number</label>
+                                <label for="phone" class="col-md-4 control-label">手机号</label>
 
                                 <div class="col-md-6">
                                     <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" required autofocus>
@@ -31,27 +31,53 @@
                                 </div>
                             </div>
 
-                            <div class="form-group{{ $errors->has('VerificationCode') ? ' has-error' : '' }}">
+                            <div id="VerificationCodeDiv" class="form-group{{ $errors->has('VerificationCode') ? ' has-error' : '' }}">
                                 <label for="VerificationCode" class="col-md-4 control-label">验证码</label>
                                 <div class="col-md-6">
                                     <input id="VerificationCode" type="text" class="form-control" name="VerificationCode" required>
-                                    <a href="#" onclick="sendVerificationCode();return false;">获取验证码</a>
-                                    <p id="status"></p>
+                                    <button id="sendVerificationCodeBtn" type="button" class="btn btn-default" onclick="sendVerificationCode();return false;">获取验证码</button>
+
+                                    <div id="VerificationCodeHelpBlock">
+                                        @if ($errors->has('VerificationCode'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('VerificationCode') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
+
                                     <script>
                                         function sendVerificationCode(){
                                             var phone = $('#phone').val();
                                             if (!phone){
-                                                alert('请输入正确的手机号!');
+                                                addErrorMsg('请输入正确的手机号!');
                                             } else {
                                                 $.post('/register/VerificationCode', {'_token': $('input[name="_token"]').val(), 'phone': phone}, function(data){
-                                                    var _status = $('#status');
                                                     if (data['status'] == 'success'){
-                                                        _status.removeClass().addClass('text-success').html('消息发送成功');
+                                                        $('#VerificationCodeDiv').removeClass("has-error");
+                                                        $('#VerificationCodeHelpBlock').empty();
+                                                        var timeToWait = <?php echo config('smsregistration.expiration', 60)?>;
+                                                        $('#sendVerificationCodeBtn').attr("disabled", true).html(timeToWait + "秒后重试");
+                                                        var timeCountDown = setInterval(function(){
+                                                            if (timeToWait > 1) {
+                                                                $('#sendVerificationCodeBtn').html(--timeToWait + "秒后重试");
+                                                            } else {
+                                                                clearInterval(timeCountDown);
+                                                                $('#sendVerificationCodeBtn').attr("disabled", false).html("发送验证码");
+                                                            }
+                                                        }, 1000);
                                                     } else {
-                                                        _status.removeClass().addClass('text-danger').html(data['message']);
+                                                        addErrorMsg(data.message)
                                                     }
                                                 }, 'JSON')
                                             }
+                                        }
+                                        function addErrorMsg(msg){
+                                            $('#VerificationCodeDiv').addClass("has-error");
+                                            $('#VerificationCodeHelpBlock').empty().append(
+                                                '<span class="help-block">'+
+                                                '<strong>' + msg + '</strong>' +
+                                                '</span>'
+                                            );
                                         }
                                     </script>
                                     @if ($errors->has('VerificationCode'))
@@ -63,7 +89,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <label for="password" class="col-md-4 control-label">密码</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control" name="password" required>
@@ -77,7 +103,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                                <label for="password-confirm" class="col-md-4 control-label">确认密码</label>
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
@@ -91,8 +117,8 @@
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Reset Password
+                                    <button type="submit" class="btn btn-success">
+                                        重置密码
                                     </button>
                                 </div>
                             </div>
